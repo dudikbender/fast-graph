fceff706781e6062136231ef1c7ca32d3707e80d
James Cornman <jcornman@365datacenters.com>
2023-02-10 18:45:05 -0600

    Revert "move main.py back where it was so Docker works"
    This reverts commit b4a2244243970a94180915792d76a1a8cae3c787.

---
+++ main.py
@@ -0,0 +1,41 @@
+# Import main FastAPI modules
+from fastapi import FastAPI, Depends
+
+# Internal packages
+from app.authorisation import auth
+from app.authorisation.auth import get_current_active_user
+from app.user_management import users
+from app.graph import crud
+from app.query import cypher
+
+app = FastAPI(title='Fast-graph',
+              description='API built for Neo4j with FastAPI',
+              version=0.1,
+              docs_url='/docs',
+              redoc_url='/redoc')
+
+app.include_router(
+    auth.router,
+    prefix='/auth',
+    tags=['Authorisation']
+)
+
+app.include_router(
+    users.router,
+    prefix='/users',
+    tags=['Users'],
+    dependencies=[Depends(get_current_active_user)]
+)
+
+app.include_router(
+    crud.router,
+    prefix='/graph',
+    tags=['Graph Objects'],
+    dependencies=[Depends(get_current_active_user)]
+)
+
+app.include_router(
+    cypher.router,
+    tags=['Query Database'],
+    dependencies=[Depends(get_current_active_user)]
+)
